70b1535a0b3038cb54f884a32fc581b379907d10
JavierDSaavedraM <javier.saavedram@udem.edu>
2025-11-01 19:10:18 -0600

    refactor: cambiar el header para que no necesite bootstrap

--- scripts/global.js
+++ scripts/global.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchToggle = document.getElementById('searchToggle');
     const searchContainer = document.getElementById('searchContainer');
-
+    const categoryToggle = document.getElementById('categoryToggle');
+    const categoryContainer = document.getElementById('categoryContainer');
     searchToggle.addEventListener('click', () => {
       searchContainer.classList.toggle('show');
+    });
+    categoryToggle.addEventListener('click', () => {
+      categoryContainer.classList.toggle('show');
     });
   });
